15e77e45e31562b1824b9a1222301f51e5c17dae
Aaron Na <syn228@nyu.edu>
2018-04-15 01:03:06 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,7 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age: "17", attribute: "worried"}, {{name: "Mercutio", age: "18", attribute: "hot-headed"}
+        {name: "Benvolio", age: "17", attribute: "worried"}, 
+        {name: "Mercutio", age: "18", attribute: "hot-headed"}
       ]
    }, 
    :capulet => {
